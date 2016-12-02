f5d84f16046af2aaa8bb99c2c309224adee7496a
John Arce <john.arce@gmail.com>
2016-12-01 20:49:48 -0800

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['memcached']['daemonize'] = true
+default['memcached']['logfile'] = '/var/log/memcached.log'
+default['memcached']['memory'] = '64'
+default['memcached']['port'] = '11211'
+default['memcached']['user'] = 'memcache'
+default['memcached']['listen_ip_address'] = '127.0.0.1'
+default['memcached']['max_connections'] = '1024'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'memcached'
+
+template '/etc/memcached.conf' do
+  source 'memcached.conf.erb'
+  notifies :restart, 'service[memcached]', :delayed
+end
+
+service 'memcached' do
+  action :enable
+end
